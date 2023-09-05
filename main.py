cd597d51ae9318d3e76f28a8f293476e83fea1a6
3itx <100333941+Looser3itx@users.noreply.github.com>
2023-09-05 02:39:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,28 +7,27 @@ def generate_gradient_word(word, start_color, end_color):
 
     # Validate input colors and word
     if not (start_color and end_color and word):
-        return "<b><color=#a3e993></color></b>"
+        return "Error"
 
     if len(start_color) != 6 or len(end_color) != 6:
-        return "<b><color=#a3e993></color></b>"
+        return "Error"
 
     try:
         start_color = tuple(int(start_color[i:i+2], 16) for i in (0, 2, 4))
         end_color = tuple(int(end_color[i:i+2], 16) for i in (0, 2, 4))
     except ValueError:
-        return "<b><color=#a3e993></color></b>"
+        return "Error"
 
-    gradient_word = "<b><color=#a3e993>"
+    gradient_word = ""
 
     word_len = len(word)
     if word_len > 0:
-        step = 1.0 / max(word_len - 1, 1)
+        step = 1.0 / max(word_len - 1, 1)  # Avoid division by zero
         for i in range(word_len):
             t = step * i
             color = interpolate_color(start_color, end_color, t)
-            gradient_word += f"{word[i]}</color><color={color}>"
+            gradient_word += f"<color={color}>{word[i]}</color>"
 
-    gradient_word += "</color></b>"
     return gradient_word
 
 if __name__ == "__main__":
@@ -37,4 +36,4 @@ if __name__ == "__main__":
     end_color = input("Enter the ending color (in hexadecimal format, e.g., 8b00ff): ")
 
     gradient_html = generate_gradient_word(input_word, start_color, end_color)
-    print("Gradient HTML:", gradient_html)
+    print("3itx SCPSL Title =>:", gradient_html)
